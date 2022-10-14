04f8b645a5b185f03565ca912d29efbce4004b07
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2022-10-14 16:10:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>/*processor dirctive*/
+int main(void)/*main function*/
+{
+	puts("Programming is like building a multilingual puzzle/n");
+	/*using puts to print*/
+		return (0);
+}
