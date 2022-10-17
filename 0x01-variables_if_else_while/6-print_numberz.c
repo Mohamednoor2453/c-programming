5cb0ad07807e6b302f3b0915e15a009244f0c7e9
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2022-10-16 18:07:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - print numbers from 0 to 10 using putchar
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
