c2d2277e9658aa5b6cbfa2076fb12eb8ba82cab1
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2022-10-16 18:40:02 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+* main - print base 16
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+	char ls;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ls = 'a'; ls <= 'f'; ls++)
+		putchar(ls);
+	putchar('\n');
+	return (0);
+}
