bbfb719a37d9615430a258af943cde7c32393942
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2022-10-18 01:35:27 -0700

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
 */
 int main(void)
 {
-	printf("size of a char: %i byte(s)/n", sizeof(char));
-	printf("size of an int: %i byte(s)/n", sizeof(int));
-	printf("size of a long: %i byte(s)/n", sizeof(long));
-	printf("size of a float: %i byte(s)", sizeof(float));
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long: %i byte(s)\n", sizeof(long));
+	printf("size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
